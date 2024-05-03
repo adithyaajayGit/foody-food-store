5678611124680e44a84c80036153d9d178f9bf91
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-03 23:47:57 +0530

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,84 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div>
-      <footer className="text-center text-white" style={{ backgroundColor: '#45637d', paddingTop: '10px', paddingBottom: '10px' }}>
-        {/* <!-- Grid container --> */}
-        <div className="container p-4">
-          {/* <!-- Section: Iframe --> */}
-          <section>
-            <div className="row d-flex justify-content-center">
-              <div className="col-lg-6">
-                <div className="ratio ratio-16x9">
-                  <iframe
-                    className="shadow-1-strong rounded"
-                    src="https://www.youtube.com/embed/WSTf9Bja7is?autoplay=1&enablejsapi=1"
-                    title="YouTube video"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              </div>
-            </div>
-          </section>
-          {/* <!-- Section: Iframe --> */}
-        </div>
-        {/* <!-- Grid container --> */}
+      <footer className="bg-body-tertiary text-center">
+        {/* Grid container */}
+        <div className="container p-4 pb-0">
+          {/* Section: Social media */}
+          <section className="mb-4">
+            {/* Facebook */}
+            <Link
+              to="#!"
+              className="btn text-white btn-floating m-1"
+              style={{ backgroundColor: '#3b5998' }}
+              role="button"
+            >
+              <i className="fab fa-facebook-f" alt="Facebook"></i>
+            </Link>
 
-        {/* <!-- Copyright --> */}
-        <div className="text-center p-2" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          © 2020 Copyright:
-          <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            {/* Twitter */}
+            <Link
+              to="#!"
+              className="btn text-white btn-floating m-1"
+              style={{ backgroundColor: '#55acee' }}
+              role="button"
+            >
+              <i className="fab fa-twitter" alt="Twitter"></i>
+            </Link>
+
+            {/* Google */}
+            <Link
+              to="#!"
+              className="btn text-white btn-floating m-1"
+              style={{ backgroundColor: '#dd4b39' }}
+              role="button"
+            >
+              <i className="fab fa-google" alt="Google"></i>
+            </Link>
+
+            {/* Instagram */}
+            <Link
+              to="#!"
+              className="btn text-white btn-floating m-1"
+              style={{ backgroundColor: '#ac2bac' }}
+              role="button"
+            >
+              <i className="fab fa-instagram" alt="Instagram"></i>
+            </Link>
+
+            {/* Linkedin */}
+            <Link
+              to="#!"
+              className="btn text-white btn-floating m-1"
+              style={{ backgroundColor: '#0082ca' }}
+              role="button"
+            >
+              <i className="fab fa-linkedin-in" alt="LinkedIn"></i>
+            </Link>
+            
+            {/* Github */}
+            <Link
+              to="#!"
+              className="btn text-white btn-floating m-1"
+              style={{ backgroundColor: '#333333' }}
+              role="button"
+            >
+              <i className="fab fa-github" alt="GitHub"></i>
+            </Link>
+          </section>
+          {/* Section: Social media */}
         </div>
-        {/* <!-- Copyright --> */}
+        {/* Grid container */}
+
+        {/* Copyright */}
+        <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+          © 2020 Copyright:
+          <Link className="text-body" to="https://mdbootstrap.com/">MDBootstrap.com</Link>
+        </div>
+        {/* Copyright */}
       </footer>
     </div>
   );
