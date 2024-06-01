c5007498958f9106c0a144b68124adcb10f383a3
Adithya Ajay <151563888+adithyaajayGit@users.noreply.github.com>
2024-06-01 23:08:27 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card() {
   return (
-    <div>
+    <div> 
     <div>
         <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
             <img className="card-img-top" src="https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg" alt="img" />
